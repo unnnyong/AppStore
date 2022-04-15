67ac4115abd4f4a73a15dc2b87fa88a9dc91a37a
unnnyong <unnnyong@gmail.com>
2022-04-15 15:11:32 +0900

    feat: add Xcode project

---
+++ AppStore/AppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreApp.swift
+//  AppStore
+//
+//  Created by Eunyeong Kim on 2022/04/15.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
